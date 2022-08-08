f98b6c2029d0a3cc6533dee2caa509c9689429a6
dev-otaher <dev.omar.taher@gmail.com>
2022-08-08 23:46:13 +0300

    Stage 2/8

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -7,8 +7,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a + b);
+        String[] input;
+        do {
+            input = scanner.nextLine().split(" ");
+            switch (input.length) {
+                case 0:
+                    continue;
+                case 1:
+                    if (input[0].equals("/exit")) {
+                        System.out.println("Bye!");
+                        break;
+                    } else {
+                        System.out.println(input[0]);
+                    }
+                    break;
+                default:
+                    int a = Integer.parseInt(input[0]);
+                    int b = Integer.parseInt(input[1]);
+                    System.out.println(a + b);
+                    break;
+            }
+        } while (!input[0].equals("/exit"));
     }
 }
