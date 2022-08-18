37044b5101cfee7fe203d590d9800ca30188174a
dev-otaher <dev.omar.taher@gmail.com>
2022-08-18 19:37:19 +0300

    Stage 5/8

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -1,20 +1,39 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    private static boolean isInvalidExpression(String expression) {
+        Matcher matcher = Pattern.compile("[a-zA-Z]").matcher(expression);
+        return expression.matches("[+-]?\\d+[+-]+") || (matcher.find() && !expression.startsWith("/"));
+    }
+
+    private static boolean isUnknownCommand(String command) {
+        return command.startsWith("/") && !command.equals("/exit") && !command.equals("/help");
+    }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
         do {
             input = scanner.nextLine();
-            
+            if (isInvalidExpression(input)) {
+                System.out.println("Invalid expression");
+                continue;
+            }
+            if (isUnknownCommand(input)) {
+                System.out.println("Unknown command");
+                continue;
+            }
             switch (input) {
+                case "":
+                    break;
                 case "/exit":
                     System.out.println("Bye!");
-                    break;
-                case "":
                     break;
                 case "/help":
                     System.out.println("The program calculates the sum/subtraction of numbers");
@@ -27,8 +46,10 @@ public class Main {
                             .replaceAll("-\\+|\\+-", "-")
                             .replaceAll(" - ", " + -");
                     int sum = 0;
-                    for (String string : input.split("\\+")) {
-                        sum += Integer.parseInt(string.trim());
+                    for (String token : input.split("\\+")) {
+                        if (!token.isEmpty()) {
+                            sum += Integer.parseInt(token.trim());
+                        }
                     }
                     System.out.println(sum);
             }
